dab46241ea064e424b1146648fe2c9aa2587b954
Joe Schäfer <schaefer@scw.info>
2017-02-15 11:56:26 +0100

    Optimize Pricerange VH

--- Classes/ViewHelpers/PricerangeViewHelper.php
+++ Classes/ViewHelpers/PricerangeViewHelper.php
@@ -15,7 +15,7 @@ class PricerangeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractView
     if(!$articles){
       return 'No articles given';
     }
-    
+
     $low = 10000000;
     $lowCurrency = $low;
     $high = 0;
@@ -24,28 +24,33 @@ class PricerangeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractView
     foreach ($articles as $key => $value) {
       if($value->getComparableprice() < $low){
         $low = $value->getComparableprice();
-        if($value->getCurrency()->getUid() > 1) {
-          $lowCurrency = $value->getCurrency()->getShort() . ' ' . $value->getPrice();
+        if($value->getCurrency()->getUid()) {
+          $lowCurrency = $value->getCurrency()->getShort();
+          $lowInUSD = $value->getCurrency()->getExchangerate() * $value->getPrice();
         }
         else {
-         $lowCurrency = false; 
+         $lowCurrency = false;
         }
       }
       if($value->getComparableprice() > $high){
         $high = $value->getComparableprice();
-        if($value->getCurrency()->getUid() > 1) {
-          $highCurrency = $value->getCurrency()->getShort() . ' ' . $value->getPrice();
+        if($value->getCurrency()->getUid()) {
+          $highCurrency = $value->getCurrency()->getShort();
+          $highInUSD = $value->getCurrency()->getExchangerate() * $value->getPrice();
         }
         else {
-         $highCurrency = false; 
+         $highCurrency = false;
         }
       }
     }
+
     if($low && $high){
       $this->templateVariableContainer->add('low', $low);
       $this->templateVariableContainer->add('high', $high);
       $this->templateVariableContainer->add('lowCurrency', $lowCurrency);
       $this->templateVariableContainer->add('highCurrency', $highCurrency);
+      $this->templateVariableContainer->add('lowInUSD', money_format('%i', $lowInUSD));
+      $this->templateVariableContainer->add('highInUSD', money_format('%i', $highInUSD));
       $output .= $this->renderChildren();
       $this->templateVariableContainer->remove('low');
       $this->templateVariableContainer->remove('high');
@@ -56,8 +61,8 @@ class PricerangeViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractView
     // $output .= parent::render();
     return $output;
   }
-  
-  
+
+
     #Want more:
     #https://docs.typo3.org/typo3cms/ExtbaseFluidBook/8-Fluid/8-developing-a-custom-viewhelper.html
 }
