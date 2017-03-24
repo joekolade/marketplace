35630e3c4a9ec4c275c4adf29f654d15776e0932
Joe Schäfer <schaefer@scw.info>
2017-03-24 14:03:14 +0100

    dev

--- Classes/ViewHelpers/AvailableListViewHelper.php
+++ Classes/ViewHelpers/AvailableListViewHelper.php
@@ -14,42 +14,32 @@ class AvailableListViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractV
    */
   public function render($product = NULL) {
 
-    $output = 'out';
+    $output = '';
 
     if(!$product){
       return 'No articles given';
     }
 
     $continents = array();
-    $countries = array();
 
     foreach ($product->getArticles() as $article) {
       $c = $article->getDealer()->getCountry()->getContinent()->getName();
-      if(!count($countries[$c])){
-        $continents[] = $c;
+      if(!count($continents[$c])){
+        $continents[$c] = array($c);
       }
-      $countries[$c][] = $article->getDealer()->getCountry()->getName();
+      $continents[$c][1][] = $article->getDealer()->getCountry()->getName();
     }
 
     \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($continents, '$continents');
-    \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($countries, '$countries');
 
+    $this->templateVariableContainer->add('continents', $continents);
 
-    // if($low && $high){
-    //   $this->templateVariableContainer->add('low', $low);
-    //   $this->templateVariableContainer->add('high', $high);
-    //   $this->templateVariableContainer->add('lowCurrency', $lowCurrency);
-    //   $this->templateVariableContainer->add('highCurrency', $highCurrency);
-    //   $this->templateVariableContainer->add('lowInUSD', money_format('%i', $lowInUSD));
-    //   $this->templateVariableContainer->add('highInUSD', money_format('%i', $highInUSD));
-    //   $output .= $this->renderChildren();
-    //   $this->templateVariableContainer->remove('low');
-    //   $this->templateVariableContainer->remove('high');
-    //   $this->templateVariableContainer->remove('lowCurrency', $lowCurrency);
-    //   $this->templateVariableContainer->remove('highCurrency', $highCurrency);
-    // }
+    $output .= $this->renderChildren();
 
-    // $output .= parent::render();
+    $this->templateVariableContainer->remove('continents');
+
+    $output .= parent::render();
+
     return $output;
   }
 
