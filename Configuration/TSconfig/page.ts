ba1e5bedb4f5b632daa7917536a392a29f042059
Joe Schäfer <schaefer@scw.info>
2017-05-16 00:21:34 +0200

    cs_seo for products

--- Configuration/TSconfig/page.ts
+++ Configuration/TSconfig/page.ts
@@ -1,24 +1,24 @@
 tx_csseo {
     # new index and table name of the model
-    1 = tx_marketplace_domain_model_product
+    2 = tx_marketplace_domain_model_product
 
     # if the get parameter is set in the URL the cs_seo properties will be shown
-    1.enable = GP:tx_marketplace_articlesearch|product
+    2.enable = GP:tx_marketplace_articlesearch|product
 
     # if the model already has fields like title etc. define them as fallback
-    1.fallback {
+    2.fallback {
 
-        # cs_seo title field fallback = mymod title field
+        # cs_seo title field fallback = product title field
         title = title
 
-        # cs_seo description field fallback = mymod description field
+        # cs_seo description field fallback = product description field
         description = description
     }
 
-    # enable evaluation for news
-    1.evaluation {
+    # enable evaluation for products
+    2.evaluation {
         # additional params to initialize the detail view, the pipe will be replaced by the uid
-        getParams = &tx_marketplace_articlesearch[controller]=Product&tx_marketplace_articlesearch[action]=Show&tx_marketplace_articlesearch[product]=|
+        getParams = &tx_marketplace_articlesearch[controller]=Product&tx_marketplace_articlesearch[action]=Show&tx_marketplace_articlesearch[product]=|&L=0
 
         # detail pid for the current records, only if set the table will be available
         detailPid = 152
