654d223b23a1309bbb41cf8f669e42f7ae3432d8
Joe Schäfer <schaefer@scw.info>
2017-05-16 00:51:39 +0200

    no more seo 4 productgroup

--- Configuration/TSconfig/page.ts
+++ Configuration/TSconfig/page.ts
@@ -14,24 +14,4 @@ tx_csseo {
         getParams = &tx_marketplace_articlesearch[action]=show&tx_marketplace_articlesearch[product]=|
         detailPid = 152
     }
-
-
-    // Productgroups
-    //
-    2 = tx_marketplace_domain_model_productgroup
-
-    3.enable = GP:tx_marketplace_articlesearch|productgroup
-
-    3.fallback {
-
-        title = name
-        description =
-    }
-
-    3.evaluation {
-        getParams = &tx_marketplace_articlesearch[controller]=Product&tx_marketplace_articlesearch[action]=list&tx_marketplace_articlesearch[productgroup]=|
-
-        # detail pid for the current records, only if set the table will be available
-        detailPid = 185
-    }
 }
