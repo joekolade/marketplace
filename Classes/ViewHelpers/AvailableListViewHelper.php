dd73df5dd11a25f781e7666e71a86ce4d4777152
Joe Schäfer <schaefer@scw.info>
2017-05-15 09:59:48 +0200

    Debug search (Producer)

--- Classes/ViewHelpers/AvailableListViewHelper.php
+++ Classes/ViewHelpers/AvailableListViewHelper.php
@@ -1,52 +1,57 @@
 <?php
+
 namespace JS\Marketplace\ViewHelpers;
 
 use \JS\Marketplace\Domain\Model\Product;
+
 // use \JS\Marketplace\Domain\Model\Article;
 
-class AvailableListViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+class AvailableListViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
 
 
-  /**
-   * @param Product $product
-   *
-   * @return string the pricerange in HTML structure
-   */
-  public function render($product = NULL) {
+    /**
+     * @param Product $product
+     *
+     * @return string the pricerange in HTML structure
+     */
+    public function render($product = null)
+    {
 
-    $output = '';
+        $output = '';
 
-    if(!$product){
-      return 'No articles given';
+        if (!$product) {
+            return 'No articles given';
+        }
+
+        $continents = array();
+
+        foreach ($product->getArticles() as $article) {
+
+            $c = $article->getDealer()->getCountry()->getContinent()->getName();
+            $country = $article->getDealer()->getCountry()->getName();
+            if (count($continents[$c])) {
+                if (!in_array($country, $continents[$c]['countries'])) {
+                    $continents[$c]['countries'][] = $country;
+                }
+            } else {
+                $continents[$c] = array(
+                    'name' => $c,
+                    'countries' => array($country)
+                );
+            }
+        }
+
+        $this->templateVariableContainer->add('continents', $continents);
+
+        $output .= $this->renderChildren();
+
+        $this->templateVariableContainer->remove('continents');
+
+        // $output .= parent::render();
+
+        return $output;
     }
-
-    $continents = array();
-
-    foreach ($product->getArticles() as $article) {
-      $c = $article->getDealer()->getCountry()->getContinent()->getName();
-      $country = $article->getDealer()->getCountry()->getName();
-      if(count($continents[$c])) {
-        if(!in_array($country, $continents[$c]['countries']))
-          $continents[$c]['countries'][] = $country;
-      }
-      else {
-        $continents[$c] = array(
-          'name' => $c,
-          'countries' => array($country)
-        );
-      }
-    }
-
-    $this->templateVariableContainer->add('continents', $continents);
-
-    $output .= $this->renderChildren();
-
-    $this->templateVariableContainer->remove('continents');
-
-    // $output .= parent::render();
-
-    return $output;
-  }
 
 
     #Want more:
