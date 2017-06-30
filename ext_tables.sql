9ffd329ad9544ca89734ee2e9ae9b649f5bb57bc
Joe Schäfer <schaefer@scw.info>
2017-06-30 17:21:48 +0200

    category and selects

--- ext_tables.sql
+++ ext_tables.sql
@@ -807,6 +807,6 @@ CREATE TABLE tx_marketplace_product_option_mm (
 #
 CREATE TABLE tx_marketplace_domain_model_select (
 
-	category int(11) unsigned DEFAULT '0' NOT NULL,
+	category int(11) unsigned DEFAULT '0' NOT NULL
 
 );
