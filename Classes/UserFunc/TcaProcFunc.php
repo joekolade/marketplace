f9bae6249e8157c655b8603eaa0c33cbdfded357
Joe Schäfer <schaefer@scw.info>
2017-06-30 16:43:53 +0200

    tca multi-masterclass

--- Classes/UserFunc/TcaProcFunc.php
+++ Classes/UserFunc/TcaProcFunc.php
@@ -31,10 +31,11 @@ class TcaProcFunc extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
     private function _getSelectsByPid($pid = 0)
     {
         $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-        $this->selectRepository = $objectManager->get('JS\\Marketplace\\Domain\\Repository\\SelectRepository');
-        $items = $this->selectRepository->findAll();
+        $selectRepository = $objectManager->get('JS\\Marketplace\\Domain\\Repository\\SelectRepository');
+        $items = $selectRepository->findAll();
 
         \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($pid);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($selectRepository);
         \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($items);
 
         return $items;
