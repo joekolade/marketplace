ad9576d1e2e8293b6034509ca1440caf410ff5cf
Joe Schäfer <schaefer@scw.info>
2017-07-03 22:50:04 +0200

    Plugin view

--- Classes/Controller/ProductController.php
+++ Classes/Controller/ProductController.php
@@ -838,7 +838,7 @@ class ProductController extends \JS\Marketplace\Controller\AbstractController
 
         $this->view->assign('filter', $filter);
         $this->view->assign('category', $category);
-        $this->view->assign('products', $this->productRepository->findHiddenByUid(1);
+        $this->view->assign('products', $this->productRepository->findHiddenByUid(1));
 
         // return;
     }
