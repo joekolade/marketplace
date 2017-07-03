a601293ed66f4f814d61dde26cf3e2fb3c5e00a5
Joe Schäfer <schaefer@scw.info>
2017-07-03 22:52:11 +0200

    Plugin view

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -155,7 +155,7 @@ plugin.tx_marketplace._CSS_DEFAULT_STYLE (
 
 plugin.tx_marketplace_ratingsearch < plugin.tx_marketplace_articlesearch
 plugin.tx_marketplace_registeruser < plugin.tx_marketplace_articlesearch
-plugin.tx_marketplace_catlist < plugin.tx_marketplace_articlesearch
+plugin.tx_marketplace_categorylist < plugin.tx_marketplace_articlesearch
 
 lib.emailCloser = COA
 lib.emailCloser {
