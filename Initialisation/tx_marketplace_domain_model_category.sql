60ae4de1666f527141957559fedd1fac53aeef6d
Joe Schäfer <schaefer@scw.info>
2017-07-03 17:12:17 +0200

    Data

---
+++ Initialisation/tx_marketplace_domain_model_category.sql
@@ -0,0 +1,8 @@
+INSERT INTO `tx_marketplace_domain_model_category` (`uid`, `pid`, `title`, `image`, `products`, `tstamp`, `crdate`, `cruser_id`, `deleted`, `hidden`, `sys_language_uid`, `l10n_parent`, `l10n_diffsource`, `selects`) VALUES
+(1,	188,	'Solar-Home-Systems',	0,	0,	1498833194,	1498828900,	1,	0,	0,	0,	0,	'a:4:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;}',	0),
+(2,	188,	'Solar Power Systems',	0,	0,	1498837530,	1498829139,	1,	0,	0,	0,	0,	'a:5:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;s:7:\"selects\";N;}',	4),
+(3,	188,	'Appliances',	0,	0,	1498833194,	1498829189,	1,	0,	0,	0,	0,	'a:4:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;}',	0),
+(4,	188,	'Light',	0,	0,	1498833194,	1498829194,	1,	0,	0,	0,	0,	'a:4:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;}',	0),
+(5,	188,	'Components',	0,	0,	1498833194,	1498829199,	1,	0,	0,	0,	0,	'a:4:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;}',	0),
+(6,	188,	'Panels',	0,	0,	1498833194,	1498829210,	1,	0,	0,	0,	0,	'a:4:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;}',	0),
+(7,	188,	'Batteries',	0,	0,	1498833194,	1498829217,	1,	0,	0,	0,	0,	'a:4:{s:16:\"sys_language_uid\";N;s:6:\"hidden\";N;s:5:\"title\";N;s:5:\"image\";N;}',	0);
