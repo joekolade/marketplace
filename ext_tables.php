6a59d700f38ed1dcfecd74766762322b500e35d1
Joe Schäfer <schaefer@scw.info>
2017-07-03 17:51:18 +0200

    Flexform für Plugin

--- ext_tables.php
+++ ext_tables.php
@@ -52,6 +52,7 @@ call_user_func(
         $pluginSignature = $extensionName . '_' . $pluginName;
         $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
         $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature,
         'FILE:EXT:' . $_EXTKEY . '/Configuration/Flexform/' . $pluginName . '_settings.xml');
 
 
