514dd7d104992cfd87c7a0e3a66194742f40b319
Joe Schäfer <schaefer@scw.info>
2017-07-03 17:50:35 +0200

    Flex

--- ext_tables.php
+++ ext_tables.php
@@ -52,6 +52,7 @@ call_user_func(
         $pluginSignature = $extensionName . '_' . $pluginName;
         $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
         $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        'FILE:EXT:' . $_EXTKEY . '/Configuration/Flexform/' . $pluginName . '_settings.xml');
 
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript',
