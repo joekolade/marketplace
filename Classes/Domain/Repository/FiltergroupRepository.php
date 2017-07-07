7a2fe690036acdb098ca9d4b2528a21acc1c8e68
Joe Schäfer <schaefer@scw.info>
2017-07-07 17:24:23 +0200

    Refactor Filtergroups
    items

--- Classes/Domain/Repository/FiltergroupRepository.php
+++ Classes/Domain/Repository/FiltergroupRepository.php
@@ -1,32 +1,6 @@
 <?php
 namespace JS\Marketplace\Domain\Repository;
 
-/***
- *
- * This file is part of the "Marketplace" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- *  (c) 2017 Joe Schäfer
- *
- ***/
-
-/**
- * The repository for Selects
- */
-class FiltergroupRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
-{
-    /**
-     * @var array
-     */
-    protected $defaultOrderings = array(
-        'sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
-    );
-
-<?php
-namespace JS\Marketplace\Domain\Repository;
-
     /***
      *
      * This file is part of the "Marketplace" Extension for TYPO3 CMS.
