1807d2c915dffd3c9c24d21d4b4642397bd2e8b8
Joe Schäfer <schaefer@scw.info>
2017-09-05 22:58:43 +0200

    New Plugins for Home
    Add sorting to cats

--- Classes/Domain/Repository/CategoryRepository.php
+++ Classes/Domain/Repository/CategoryRepository.php
@@ -36,8 +36,8 @@ class CategoryRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
      * @var array
      */
     protected $defaultOrderings = array(
-        'title' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
-        //'sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
+        'sorting' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+        'title' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
     );
 
 }
